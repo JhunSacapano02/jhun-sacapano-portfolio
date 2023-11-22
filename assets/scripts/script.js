eae4ec79e65fe6a6db567905a4a1dfbf09da25fd
JhunSacapano02 <jhunsacapano02@gmail.com>
2023-11-22 20:47:30 +0800

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,27 @@
+
+//dom manipulation
+// const body = document.body
+// body.append("hello world", "hello again")
+
+
+// const div = document.createElement("div")
+// div.innerHTML = "<strong>Web Dev</strong>"
+// body.append(div)
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// let i = 1;
+
+// while (i <= 100) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
